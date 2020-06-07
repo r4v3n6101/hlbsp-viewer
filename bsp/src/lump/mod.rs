8e1851c222878da59aad8a7edd380c96cd531126
r4v3n6101 <raven6107@gmail.com>
2020-06-07 23:58:27 +0300

    just commit

--- bsp/src/lump/mod.rs
+++ bsp/src/lump/mod.rs
@@ -1,23 +1,48 @@
-use cgmath::prelude;
+use byteorder::{ReadBytesExt, LE};
+use cgmath::Point3;
 use std::{
-    ffi::{CStr, CString},
-    io::{Error as IOError, ErrorKind, Read, Result as IOResult},
+    ffi::CString,
+    io::{Cursor, Result as IOResult},
+    mem::size_of,
 };
 
-pub trait LumpReader {
-    type Output;
-
-    fn read(data: Vec<u8>) -> IOResult<Self::Output>;
+pub fn read_entities(data: Vec<u8>) -> IOResult<CString> {
+    // TODO : wrap Vec<u8> directly to CString (waitin for issue solving)
+    let mut data = data;
+    data.pop();
+    Ok(CString::new(data)?)
 }
 
-pub struct Entity;
-
-impl LumpReader for Entity {
-    type Output = CString;
-
-    fn read(data: Vec<u8>) -> IOResult<Self::Output> {
-        CStr::from_bytes_with_nul(&data)
-            .map_err(|e| IOError::new(ErrorKind::InvalidData, e))
-            .map(|v| v.to_owned())
+type Point = Point3<f32>;
+pub fn read_vertices(data: Vec<u8>) -> IOResult<Vec<Point>> {
+    let len = data.len() / size_of::<Point>();
+    let mut cursor = Cursor::new(data);
+    let mut vertices = Vec::with_capacity(len);
+    for _ in 0..len {
+        let x = cursor.read_f32::<LE>()?;
+        let y = cursor.read_f32::<LE>()?;
+        let z = cursor.read_f32::<LE>()?;
+        vertices.push(Point::new(x, y, z));
     }
+    Ok(vertices)
+}
+
+pub struct Face {
+    plane: u16,
+    plane_side: u16,
+    first_edge: u16,
+    edges: u16,
+    texinfo: u16,
+    styles: [u8; 4],
+    lightmap_offset: u32,
+}
+
+pub fn read_faces(data: Vec<u8>) -> IOResult<Vec<Face>> {
+    let len = data.len() / size_of::<Point>();
+    let mut cursor = Cursor::new(data);
+    let mut faces = Vec::with_capacity(len);
+    for _ in 0..len {
+        todo!()
+    }
+    Ok(faces)
 }
