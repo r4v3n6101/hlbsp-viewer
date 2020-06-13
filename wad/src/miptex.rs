e022ce0ad8927327f0f1cf997204b9e8dd2f88c6
r4v3n6101 <raven6107@gmail.com>
2020-06-13 23:03:05 +0300

    MipTexture optional offsets

--- wad/src/miptex.rs
+++ wad/src/miptex.rs
@@ -1,10 +1,11 @@
 use crate::name::deserialize_fixed_len_cstring;
-use arraylib::Array;
+use arraylib::{Array, ArrayMap};
 use bincode::{deserialize_from, Result as BincodeResult};
 use serde::Deserialize;
 use std::{
     ffi::CString,
     io::{Cursor, Read, Seek, SeekFrom},
+    num::NonZeroU32,
 };
 
 const MIP_TEXTURES: usize = 4;
@@ -13,22 +14,23 @@ pub struct MipTexture {
     pub name: CString,
     pub width: u32,
     pub height: u32,
-    pub color_indices: [Vec<u8>; MIP_TEXTURES],
-    pub color_table: Palette,
+    pub color_indices: [Option<Vec<u8>>; MIP_TEXTURES],
+    pub color_table: Option<Palette>,
 }
 
 pub type Palette = [u8; 256 * 3];
 
 impl MipTexture {
     pub fn new(data: Vec<u8>) -> BincodeResult<MipTexture> {
-        #[derive(Debug, Deserialize)]
+        #[derive(Deserialize)]
         struct MipTexHeader {
             #[serde(deserialize_with = "deserialize_fixed_len_cstring")]
             name: CString,
             width: u32,
             height: u32,
-            offsets: [u32; 4],
+            offsets: [u32; MIP_TEXTURES],
         }
+
         let mut cursor = Cursor::new(data);
         let MipTexHeader {
             name,
@@ -36,19 +38,29 @@ impl MipTexture {
             height,
             offsets,
         } = deserialize_from(&mut cursor)?;
+        let offsets: [Option<NonZeroU32>; MIP_TEXTURES] = offsets.map(|x| NonZeroU32::new(x));
 
-        let mut color_indices = <[_; MIP_TEXTURES]>::from_fn(|_| vec![]);
+        // TODO : check that all offsets exist otherwise texture will be unloaded
+        let color_table = if let Some(last_offset) = offsets[MIP_TEXTURES - 1] {
+            cursor.seek(SeekFrom::Start(last_offset.get() as u64 + 2))?;
+            let mut color_table = [0u8; 256 * 3];
+            cursor.read_exact(&mut color_table)?;
+            Some(color_table)
+        } else {
+            None
+        };
+
+        let mut color_indices = <[_; MIP_TEXTURES]>::from_fn(|_| None);
         for i in 0..MIP_TEXTURES {
-            let offset = offsets[i];
-            cursor.seek(SeekFrom::Start(offset as u64))?;
-            let size = width * height / (1 << (2 * i));
-            let mut indices = vec![0u8; size as usize];
-            cursor.read_exact(&mut indices)?;
-            color_indices[i] = indices;
+            if let Some(offset) = offsets[i] {
+                let offset = offset.get();
+                cursor.seek(SeekFrom::Start(offset as u64))?;
+                let size = width * height / (1 << (2 * i));
+                let mut indices = vec![0u8; size as usize];
+                cursor.read_exact(&mut indices)?;
+                color_indices[i] = Some(indices);
+            }
         }
-        cursor.seek(SeekFrom::Current(2))?; // skip 2 dummy bytes after last mipdata
-        let mut color_table = [0u8; 256 * 3];
-        cursor.read_exact(&mut color_table)?;
 
         Ok(MipTexture {
             name,
