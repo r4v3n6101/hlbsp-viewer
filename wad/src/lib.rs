8d94ce6c055200d91ca88c849a444337e3a9f64c
r4v3n6101 <raven6107@gmail.com>
2020-07-11 23:10:37 +0300

    wad: serde -> nom

--- wad/src/lib.rs
+++ wad/src/lib.rs
@@ -1,3 +1,93 @@
-pub mod io;
-pub mod miptex;
-mod name;
+use nom::{
+    bytes::streaming::{tag, take, take_until},
+    combinator::{map, map_res},
+    multi::count,
+    number::streaming::{le_u16, le_u32, le_u8},
+    sequence::tuple,
+};
+use std::{collections::HashMap, iter::Iterator};
+
+const WAD3_MAGIC: &[u8] = b"WAD3";
+const NAME_LEN: usize = 16;
+
+type Input<'a> = &'a [u8];
+type ParseError<'a> = nom::error::VerboseError<Input<'a>>;
+type ParseResult<'a, O> = nom::IResult<Input<'a>, O, ParseError<'a>>;
+
+fn take_cstr(i: &[u8], size: usize) -> ParseResult<&str> {
+    let (i, cstr) = take(size)(i)?;
+    let (_, cstr) = map_res(take_until("\0"), std::str::from_utf8)(cstr)?;
+    Ok((i, cstr))
+}
+
+pub struct Entry<'a> {
+    etype: u8,
+    data: &'a [u8],
+}
+
+impl<'a> Entry<'a> {
+    pub const fn etype(&self) -> u8 {
+        self.etype
+    }
+
+    pub const fn data(&self) -> &[u8] {
+        self.data
+    }
+
+    fn parse(i: &'a [u8], file: &'a [u8]) -> ParseResult<'a, (&'a str, Entry<'a>)> {
+        // There's no compression, because I don't find any wad using compression (seems it's LZSS)
+        let (i, (offset, disk_size, _, etype, _, _)) = tuple((
+            map(le_u32, |x| x as usize),
+            map(le_u32, |x| x as usize),
+            le_u32,
+            le_u8,
+            le_u8,
+            le_u16,
+        ))(i)?;
+        let (i, name) = take_cstr(i, NAME_LEN)?;
+
+        let data_i = {
+            if offset > file.len() {
+                return Err(nom::Err::Incomplete(nom::Needed::Size(offset))); // TODO : not verbose error
+            }
+            &file[offset..]
+        };
+        let (_, data) = take(disk_size)(data_i)?;
+
+        Ok((i, (name, Entry { etype, data })))
+    }
+}
+
+pub struct Archive<'a> {
+    entries: HashMap<&'a str, Entry<'a>>,
+}
+
+impl Archive<'_> {
+    pub fn entries(&self) -> impl Iterator<Item = (&&str, &Entry)> {
+        self.entries.iter()
+    }
+
+    pub fn get_by_name(&self, name: &str) -> Option<&Entry> {
+        self.entries.get(name)
+    }
+
+    pub fn parse(file: &[u8]) -> Result<Archive, nom::Err<ParseError<'_>>> {
+        let (_, (_, dir_num, dir_offset)) = tuple((
+            tag(WAD3_MAGIC),
+            map(le_u32, |x| x as usize),
+            map(le_u32, |x| x as usize),
+        ))(file)?;
+
+        let dir_i = {
+            if dir_offset > file.len() {
+                return Err(nom::Err::Incomplete(nom::Needed::Size(dir_offset)));
+            }
+            &file[dir_offset..]
+        };
+
+        let (_, entries) = map(count(|i| Entry::parse(i, file), dir_num), |x| {
+            x.into_iter().collect()
+        })(dir_i)?;
+        Ok(Archive { entries })
+    }
+}
--- wad/tests/wad_read.rs
+++ wad/tests/wad_read.rs
@@ -1,20 +1,6 @@
 #[test]
-fn check_wad_entries_length() {
-    let data = std::io::Cursor::new(std::fs::read(env!("WAD_TEST")).unwrap());
-    let wad_reader = wad::io::WadReader::create(data).unwrap();
-    let entries = wad_reader.entries();
-    assert_eq!(entries.len(), 3116);
-}
-
-#[test]
-fn print_wad_entry_names() {
-    let data = std::io::Cursor::new(std::fs::read(env!("WAD_TEST")).unwrap());
-    let wad_reader = wad::io::WadReader::create(data).unwrap();
-    wad_reader.entries().iter().for_each(|data| {
-        let wad_name_upper = data.name().to_str().unwrap();
-        let data = wad_reader.read_entry(data).unwrap();
-        let miptex = wad::miptex::MipTexture::new(&data).unwrap();
-        let miptex_name = miptex.name.to_str().unwrap();
-        assert_eq!(wad_name_upper.to_lowercase(), miptex_name.to_lowercase());
-    });
+fn print_entries_name() {
+    let file = std::fs::read(env!("WAD_TEST")).unwrap();
+    let wad = wad::Archive::parse(&file).unwrap();
+    wad.entries().for_each(|(&name, _)| println!("{}", name));
 }
