1b59339aaa7a04718ce13ea830738b4dff33103b
r4v3n6101 <raven6107@gmail.com>
2020-07-12 16:49:29 +0300

    add bsp test

--- bsp/tests/bsp_read.rs
+++ bsp/tests/bsp_read.rs
@@ -7,3 +7,13 @@ fn print_vertices_num() {
         map.lump_data(bsp::LumpType::Vertices).len() / (4 * 3)
     );
 }
+
+#[test]
+fn print_entities_lump() {
+    let file = std::fs::read(env!("BSP_TEST")).unwrap();
+    let map = bsp::Map::parse(&file).unwrap();
+    println!(
+        "Entities: {}",
+        std::str::from_utf8(map.lump_data(bsp::LumpType::Entities)).unwrap()
+    );
+}
