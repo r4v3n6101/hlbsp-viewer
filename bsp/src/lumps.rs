ed96301e88d4eea265ee129fc6f8693f1c33e7ff
r4v3n6101 <raven6107@gmail.com>
2020-07-15 15:09:04 +0300

    bsp parsers more

--- bsp/src/lumps.rs
+++ bsp/src/lumps.rs
@@ -2,12 +2,29 @@ use nom::{
     bytes::complete::take_until,
     combinator::{map, map_res},
     multi::many0,
-    number::complete::{le_f32, le_u16, le_u32, le_u8},
+    number::complete::{le_f32, le_i16, le_u16, le_u32, le_u8},
     sequence::tuple,
 };
 
 pub type Vec3 = (f32, f32, f32);
 type ParseResult<'a, O> = Result<O, nom::Err<(&'a [u8], nom::error::ErrorKind)>>;
+
+pub struct TexInfo {
+    pub vs: Vec3,
+    pub ss: f32,
+    pub vt: Vec3,
+    pub st: f32,
+    pub miptex: usize,
+}
+
+pub struct Face {
+    pub plane_id: usize,
+    pub side: bool,
+    pub ledge_id: usize,
+    pub ledge_num: usize,
+    pub texinfo_id: usize,
+    pub lightmap: usize,
+}
 
 pub fn parse_entities_str(i: &[u8]) -> ParseResult<&str> {
     let (_, s) = map_res(take_until("\0"), std::str::from_utf8)(i)?;
@@ -23,6 +40,20 @@ pub fn parse_vertices(i: &[u8]) -> ParseResult<Vec<Vec3>> {
     Ok(vertices)
 }
 
+pub fn parse_edge(i: &[u8]) -> nom::IResult<&[u8], (usize, usize)> {
+    tuple((map(le_u32, |x| x as usize), map(le_u32, |x| x as usize)))(i)
+}
+
+pub fn parse_edges(i: &[u8]) -> ParseResult<Vec<(usize, usize)>> {
+    let (_, edges) = many0(parse_edge)(i)?;
+    Ok(edges)
+}
+
+pub fn parse_ledges(i: &[u8]) -> ParseResult<Vec<i16>> {
+    let (_, ledges) = many0(le_i16)(i)?;
+    Ok(ledges)
+}
+
 pub fn parse_normal_from_plane(i: &[u8]) -> nom::IResult<&[u8], Vec3> {
     let (i, (normal, _, _)) = tuple((parse_vec3, le_f32, le_u32))(i)?;
     Ok((i, normal))
@@ -33,7 +64,7 @@ pub fn parse_normals_from_planes(i: &[u8]) -> ParseResult<Vec<Vec3>> {
     Ok(normals)
 }
 
-pub fn parse_texinfo(i: &[u8]) -> nom::IResult<&[u8], ()> {
+pub fn parse_texinfo(i: &[u8]) -> nom::IResult<&[u8], TexInfo> {
     let (i, (vs, ss, vt, st, miptex, _)) = tuple((
         parse_vec3,
         le_f32,
@@ -42,15 +73,24 @@ pub fn parse_texinfo(i: &[u8]) -> nom::IResult<&[u8], ()> {
         map(le_u32, |x| x as usize),
         le_u32,
     ))(i)?;
-    todo!()
+    Ok((
+        i,
+        TexInfo {
+            vs,
+            ss,
+            vt,
+            st,
+            miptex,
+        },
+    ))
 }
 
-pub fn parse_texinfos(i: &[u8]) -> ParseResult<Vec<()>> {
+pub fn parse_texinfos(i: &[u8]) -> ParseResult<Vec<TexInfo>> {
     let (_, texinfos) = many0(parse_texinfo)(i)?;
     Ok(texinfos)
 }
 
-pub fn parse_face(i: &[u8]) -> nom::IResult<&[u8], ()> {
+pub fn parse_face(i: &[u8]) -> nom::IResult<&[u8], Face> {
     let (i, (plane_id, side, ledge_id, ledge_num, texinfo_id, _, _, _, _, lightmap)) = tuple((
         map(le_u16, |x| x as usize),
         map(le_u16, |x| x != 0),
@@ -63,10 +103,20 @@ pub fn parse_face(i: &[u8]) -> nom::IResult<&[u8], ()> {
         le_u8,
         map(le_u32, |x| x as usize),
     ))(i)?;
-    todo!()
+    Ok((
+        i,
+        Face {
+            plane_id,
+            side,
+            ledge_id,
+            ledge_num,
+            texinfo_id,
+            lightmap,
+        },
+    ))
 }
 
-pub fn parse_faces(i: &[u8]) -> ParseResult<Vec<()>> {
+pub fn parse_faces(i: &[u8]) -> ParseResult<Vec<Face>> {
     let (_, faces) = many0(parse_face)(i)?;
     Ok(faces)
 }
