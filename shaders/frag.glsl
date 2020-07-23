a4c409f240f46eccf264277bea889cb8a3bd3460
r4v3n6101 <raven6107@gmail.com>
2020-07-23 15:18:58 +0300

    simple lighting

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -1,7 +1,15 @@
 #version 140
+
 in vec2 o_tex_coords;
+in vec3 o_normal;
+
 uniform sampler2D tex;
 
+const vec3 LIGHT = vec3(0.2, -0.8, 0.1);
+
 void main() {
-    gl_FragColor = texture2D(tex, o_tex_coords);
+    float lum = max(dot(normalize(o_normal), normalize(LIGHT)), 0.0);
+    vec3 color = (0.3 + 0.7 * lum) * vec3(1.0, 1.0, 1.0);
+    vec4 tex_color = texture2D(tex, o_tex_coords);
+    gl_FragColor = vec4(tex_color.xyz * color, tex_color.w);
 }
--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -2,16 +2,20 @@
 
 in vec3 position;
 in vec2 tex_coords;
+in vec3 normal;
 
 out vec2 o_tex_coords;
+out vec3 o_normal;
 
 uniform mat4 proj;
 uniform mat4 view;
 uniform vec3 origin;
 
 void main() {
-    o_tex_coords =  tex_coords;
-    vec3 pos = (position - origin) * 0.001;
+    o_tex_coords = tex_coords;
+    o_normal = normal;
+
+    vec3 pos = (position - origin) * 0.003;
     gl_Position = proj * view * vec4(vec3(pos.x, pos.z, -pos.y), 1.0);
 }
 
