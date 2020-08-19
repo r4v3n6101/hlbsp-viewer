c9e4f6b01e365531c1547e07a63720b00988b177
r4v3n6101 <raven6107@gmail.com>
2020-08-19 22:19:42 +0300

    Rename const

--- file/src/cubemap.rs
+++ file/src/cubemap.rs
@@ -2,7 +2,7 @@ use arraylib::ArrayMap;
 use std::path::Path;
 
 const EXTENSION: &str = "tga";
-const SKYNAME_POSTFIX: [&str; 6] = ["rt", "lf", "up", "dn", "ft", "bk"];
+const SIDES: [&str; 6] = ["rt", "lf", "up", "dn", "ft", "bk"];
 
 pub struct Cubemap {
     dimension: u32,
@@ -12,7 +12,7 @@ pub struct Cubemap {
 impl Cubemap {
     pub fn read<S: AsRef<str>, P: AsRef<Path>>(name: S, path: P) -> Self {
         let mut dimension = 0;
-        let sides = SKYNAME_POSTFIX.map(|postfix| {
+        let sides = SIDES.map(|postfix| {
             let file_name = format!("{}{}.{}", name.as_ref(), postfix, EXTENSION);
             let file_path = path.as_ref().join(file_name);
             let image = image::open(file_path).unwrap().to_rgba(); // TODO : remove unwrap
